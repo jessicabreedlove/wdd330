a2d74d01df4a41a459a945c9159c9c5db4e2d408
jessicabreedlove <jessicabreedlove@gmail.com>
2022-04-19 20:46:48 -0600

    working list

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const myProjects = ["Project 1", "Project 2"];
+const myProjects = [
+  { label: "Project 1 ", url: "week1/index.html" },
+  { label: "Project 2 ", url: "week2/index.html" },
+];
 myProjects.forEach(listItem);
 
 function listItem(listOfProjects) {
+  const projects = document.getElementById("projectList");
   let project = document.createElement("li");
-  project.innerHTML = listOfProjects;
-  document.getElementById("projectList").appendChild(project);
+  let projectA = document.createElement("a");
+  projectA.innerHTML = listOfProjects.url;
+  projectA.setAttribute("href", listOfProjects.url);
+  project.innerHTML = listOfProjects.label;
+  project.appendChild(projectA);
+  projects.appendChild(project);
 }
