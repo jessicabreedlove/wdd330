f07f700d877097909333091d5b302fdccaae57d1
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-24 13:44:54 -0600

    local storage progress

---
+++ todo/js/ls.js
@@ -0,0 +1,24 @@
+//window.localStorage.setItem('name, "Karl"):
+//updateUI();
+
+//putting in to local storage - use JSON.stringify
+//retrieving object from local storage - use JSON.parse
+
+//.setItem, .getItem, .removeItem, .clear
+
+// Local Storage Helpers
+
+//add to local storage
+document.getElementById('addBtn').addEventListener('click', () => {
+  window.localStorage.setItem(
+    'items',
+    JSON.stringify({ name: 'Jess', age: '28' })
+  );
+  updateUI();
+});
+
+//get local storage
+document.getElementById('addBtn').addEventListener('click', () => {
+  const user = JSON.parse(localStorage.getItem('users'));
+  document.getElementById('itemInList').textContent = user;
+});
