1538376595be0c18bead1a9563b923771e9adc74
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-26 21:37:19 -0600

    teacher helped

--- w6_todo/js/main.js
+++ w6_todo/js/main.js
@@ -8,26 +8,81 @@ window.onload = () => {
   const year = date.getFullYear();
 
   dateBox.append(`Date : ${month}/${day}/${year}`);
+  rebuildHTML();
 };
 
 const formTodo = document.querySelector('.formToDo');
 let list = document.querySelector('.list');
 
-//adding items to the list
-if (JSON.parse(localStorage.getItem('todoItemsTut')) !== null) {
-  JSON.parse(localStorage.getItem('todoItemsTut')).map((todo) => {
-    const div = document.createElement('div');
-    div.className = 'item';
-    const input = document.createElement('input');
-    input.type = 'checkbox';
-    input.name = 'itemCheck';
-    input.id = todo.id;
-    const p = document.createElement('p');
-    p.innerHTML = todo.name;
-    div.append(input);
-    div.append(p);
-    list.append(div);
-  });
+let activeBtn = document.getElementById('filterActive');
+let completedBtn = document.getElementById('filterComplete');
+let allBtn = document.getElementById('filterAll');
+
+activeBtn.addEventListener('click', () => {
+  rebuildHTML('active');
+});
+
+completedBtn.addEventListener('click', () => {
+  rebuildHTML('completed');
+});
+
+allBtn.addEventListener('click', () => {
+  rebuildHTML('');
+});
+
+function rebuildHTML(filter) {
+  //adding items to the list
+  list.innerHTML = '';
+  let localList = getLocal();
+  if (localList !== null) {
+    localList
+      .filter((todo) => {
+        if (filter == 'active') {
+          if (todo.completed == true) {
+            return false;
+          }
+        } else if (filter == 'completed') {
+          if (todo.completed == false) {
+            return false;
+          }
+        }
+        return true;
+      })
+      .map((todo) => {
+        const div = document.createElement('div');
+        div.className = 'item';
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.name = 'itemCheck';
+        input.checked = todo.completed;
+        input.id = todo.id;
+        input.addEventListener('click', () => {
+          //move item from active to completed - also in local
+          localList = localList.map((localItem) => {
+            if (localItem.id == todo.id) {
+              localItem.completed = input.checked;
+            }
+            return localItem;
+          });
+          saveLocal(localList);
+        });
+        const p = document.createElement('p');
+        p.innerHTML = todo.name;
+        div.append(input);
+        div.append(p);
+        list.append(div);
+      });
+  }
+}
+
+//function to stringify
+function saveLocal(items) {
+  localStorage.setItem('todoItemsTut', JSON.stringify(items));
+}
+
+//function to parse
+function getLocal() {
+  return JSON.parse(localStorage.getItem('todoItemsTut'));
 }
 
 //removing item from list and localStorage
@@ -73,6 +128,7 @@ formTodo.onsubmit = function (e) {
   let item = {
     id: id,
     name: value,
+    completed: false,
   };
   if (value == '') {
     alert('please add a value');
@@ -80,14 +136,14 @@ formTodo.onsubmit = function (e) {
     if (JSON.parse(localStorage.getItem('todoItemsTut')) == null) {
       items.push(item);
       localStorage.setItem('todoItemsTut', JSON.stringify(items));
-      window.location.reload();
+      rebuildHTML();
     } else {
       JSON.parse(localStorage.getItem('todoItemsTut')).map((todo) => {
         items.push(todo);
       });
       items.push(item);
       localStorage.setItem('todoItemsTut', JSON.stringify(items));
-      window.location.reload();
+      rebuildHTML();
     }
   }
 };
