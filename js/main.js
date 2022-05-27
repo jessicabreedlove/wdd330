ad0fb05d72c549d88396c674b067cd4cf7b50f82
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-27 17:46:47 -0600

    nicer portfolio page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const myProjects = [
-  { label: "Project 1 - Story Editor ", url: "week1/story_editor.html" },
-  { label: "Project 2 ", url: "week1/story_editor.html" },
+  { label: 'Project 1 - Story Editor ', url: 'week1/story_editor.html' },
+  { label: 'Project 2 - Todo List ', url: 'w6_todo/index.html' },
 ];
 myProjects.forEach(listItem);
 
 function listItem(listOfProjects) {
-  const projects = document.getElementById("projectList");
-  let project = document.createElement("li");
-  let projectA = document.createElement("a");
+  const projects = document.getElementById('projectList');
+  let project = document.createElement('li');
+  let projectA = document.createElement('a');
   projectA.innerHTML = listOfProjects.url;
-  projectA.setAttribute("href", listOfProjects.url);
+  projectA.setAttribute('href', listOfProjects.url);
   project.innerHTML = listOfProjects.label;
   project.appendChild(projectA);
   projects.appendChild(project);
